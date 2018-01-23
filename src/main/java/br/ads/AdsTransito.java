768bd29a820a41f62b60c7df5c3e33e683f276fa
Douglas Maitelli <dougmaitelli@gmail.com>
2018-01-23 10:46:28 +0100

    Trigger Build

--- src/main/java/br/ads/AdsTransito.java
+++ src/main/java/br/ads/AdsTransito.java
@@ -8,9 +8,6 @@ import br.ads.view.RuaWindow;
  */
 public class AdsTransito {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         RuaWindow r = new RuaWindow();
         r.setVisible(true);
